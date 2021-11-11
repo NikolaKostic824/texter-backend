da327a38cb91f2050f16a1dc12cb7a73f907406b
NikolaKostic824 <nikola.kostic8248@gmail.com>
2021-11-12 00:06:18 +0100

    make it better

--- controllers/mim.js
+++ controllers/mim.js
@@ -2,9 +2,10 @@ import Mim from '../model/mim.js';
 
 //Create new mim
 export const createMim = async (req, res) => {
-    const mim = req.body;
-    const newMim = new Mim(mim);
+    
     try {
+        const mim = req.body;
+        const newMim = new Mim(mim);
         console.log(req)
         await newMim.save()
         res.status(201).json({
