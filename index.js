b5d0322a08eb27553f861951b27f2a0b8dc4205d
NikolaKostic824 <nikola.kostic8248@gmail.com>
2021-11-11 20:49:44 +0100

    make it better

--- index.js
+++ index.js
@@ -21,15 +21,14 @@ app.use(express.urlencoded({
 }));
 
 //CORS
-app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+const corsOpts = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
+    allowedHeaders: ['Content-Type'],
+    exposedHeaders: ['Content-Type']
+};
+app.use(cors(corsOpts));
 //App routes
 app.use('/mim', mimRoutes);
 app.use('/user', userRoutes);
