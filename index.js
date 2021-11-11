d890b6512b793d939ba1eef4f7a48907b98a6b08
NikolaKostic824 <nikola.kostic8248@gmail.com>
2021-11-11 21:29:52 +0100

    make it better

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ import textRoutes from './routes/text.js'
 // Express creation
 const app = express();
 dotenv.config();
+//CORS
+app.use(cors());
 app.use(express.json({
     limit: "30mb",
     extended: true
@@ -29,8 +31,7 @@ app.use('/text', textRoutes);
 app.get('/',(req,res) => {
     res.send('Hello from API');
 })
-//CORS
-app.use(cors());
+
 //Connection
 //const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
