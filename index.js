e33784cb57a4ee51908a52b2871402817d3312bb
NikolaKostic824 <nikola.kostic8248@gmail.com>
2021-11-12 09:28:54 +0100

    make it better aj sad

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({
 }));
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
