a329d732894b963c8f25add5aa7fd4021d537080
NikolaKostic824 <nikola.kostic8248@gmail.com>
2021-11-12 01:47:14 +0100

    make it better aj sad

--- controllers/mim.js
+++ controllers/mim.js
@@ -2,16 +2,13 @@ import Mim from '../model/mim.js';
 
 //Create new mim
 export const createMim = async (req, res) => {
+    const mim = req.body;
+    const newMim = new Mim(mim);
     try {
-        const mim = req.body;
-        const newMim = new Mim(mim);
-
         await newMim.save()
-        res.status(201).json({
+        res.json({
             message: 'Post saved'
         });
-        res.end();
-
     } catch (error) {
         res.status(409).json({
             message: error.message
@@ -20,7 +17,7 @@ export const createMim = async (req, res) => {
 }
 
 //Get all mims
-export const getMims = async (req, res) => {
+export const getMims = async (req,res) => {
     try {
         const mims = await Mim.find();
         res.status(200).json(mims)
